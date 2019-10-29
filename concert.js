28d4e756f5e79f745d8b3da2024558cfca4835f4
Dan Gallagher <dagallagher221@gmail.com>
2019-10-28 21:27:51 -0400

    added depndencies and finished concerts/movies

--- concert.js
+++ concert.js
@@ -1,9 +1,10 @@
+//Requires keys from keys.js
 var keys = require("./keys");
-
+//Requires access to file system
 var fs = require("fs");
-
+//Requires axios for the axios calls
 var axios = require("axios");
-
+//Requires moment for date/time manipulation
 var moment = require('moment');
 
 
@@ -14,10 +15,23 @@ function myConcert(userInput) {
     axios.get(url).then(
         function(response) {
             for (var i = 0; i < response.data.length; i++) {
+                //Search the bands in town api and find concert data, populate it on screen
+                //convert concert time to a readable format via moment
                 console.log("Concert Time: " + moment(response.data[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A'));
                 console.log("Concert Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
-                
+                console.log("Concert Venue: " + response.data[i].venue.name);
+                console.log("-----------------------------------------");
+
+                //Append data to log.txt
+                fs.appendFileSync('log.txt', "\r\n" + "Concert Search Log---------------------------" + "\r\n" + 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "Venue Name: " + response.data[i].venue.name + "\r\n" + 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "Venue Locations" + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country + "\r\n" + 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "Venue Time: " + moment(response.data[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A') + "\r\n" + 'utf8' );
+                fs.appendFileSync('log.txt', "\r\n" + "---------------------------------------------" + "\r\n" + 'utf8');
+
             }
         }
-    )
-}
+    );
+};
+
+module.exports = myConcert;
